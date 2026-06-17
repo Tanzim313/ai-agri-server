c34136cc7f5694d1d4531ef016a67643faef7432
Tanzim313 <tanzimhasanrizbi@gmail.com>
2026-06-18 00:05:06 +0600

    frist

---
+++ server.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+
+const express = require("express");
+const { connectDB, getDB } = require("./db");
+
+const app = express();
+
+app.use(express.json());
+
+async function startServer() {
+  await connectDB();
+
+  const db = getDB();
+  const userCollection = db.collection("user");
+
+
+
+  app.get("/users", async (req, res) => {
+  const db = getDB();
+
+  const users = await userCollection
+    .find({})
+    .toArray();
+
+  res.send(users);
+ });
+
+
+
+  app.get("/", (req, res) => {
+    res.json({
+      success: true,
+      message: "Server Running",
+    });
+  });
+
+
+  app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server running on port ${process.env.PORT || 5000}`);
+  });
+}
+
+startServer();
